aff7c76031171049cfec5f200458b3e5ef8902f4
Akbar Kantor <akbaragungtp@gmail.com>
2021-10-15 10:50:14 +0700

    commit

--- application/views/operasional/accrual_baru.php
+++ application/views/operasional/accrual_baru.php
@@ -22,20 +22,15 @@
                                       <th>Angsuran / Bulan </th>
                                       <?php
 $tglawal = date('Y-01');
-$flagtanggal = date('2095-09');
+
 foreach ($bulanmax as $u) {
 
-    for ($i = 0; $i <= $u->DJPDjangkawaktu && $flagtanggal = date('Y-M', strtotime($u->DJPDtanggalakhir)); $i++) {?>
-
-                                      <th> <?php
-
+    for ($i = 0; $i <= $u->DJPDjangkawaktu; $i++) {
         $tglakhir = date('Y-M', strtotime('+ ' . $i . 'month', strtotime($tglawal)));
-        echo $tglakhir;
+        ?><th>  </th><?php
+}
 
-        $flagtanggal = $tglakhir;}
-
-}?></th>
-
+}?>
                                   </tr>
                               </thead>
                               <tbody class="text-dark">
