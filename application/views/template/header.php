eda28040e02cbb2dbc142d9255ba47aa158239fa
Akbar Kantor <akbaragungtp@gmail.com>
2021-11-18 17:10:14 +0700

    asd

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -17,6 +17,7 @@
     </script>
     <script src="<?=base_url('assets/');?>vendors/scripts/core.js"></script>
     <script src="<?=base_url('assets/');?>vendors/scripts/script.min.js"></script>
+   
     <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('assets/');?>vendors/images/logo-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('assets/');?>vendors/images/logo-icon.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?=base_url('assets/');?>vendors/images/logo-icon.png">
